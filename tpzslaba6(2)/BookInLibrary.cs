22a027a4e69f43cdd518dc104acd0c9751dce7be
tenickajaalena <tenickajaalena@gmail.com>
2019-05-23 16:04:46 +0300

    Добавьте файлы проекта.

---
+++ tpzslaba6(2)/BookInLibrary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpzslaba6_2_
+{
+    class BookInLibrary: IComparable
+    {
+        string Name { get; }
+        string Title { get; }
+        int Code { get; set; }
+        int Year { get; }
+        int NumPages { get; }
+        string Genre { get; }
+        public BookInLibrary() { }
+        public BookInLibrary(string name, string title, int code, int year, int numPages, string genre)
+        {
+            Name = name;
+            Title = title;
+            Code = code;
+            Year = year;
+            NumPages = numPages;
+            Genre = genre;
+        }
+        public int CompareTo(object o)
+        {
+            BookInLibrary p = o as BookInLibrary;
+            if (p != null)
+                return this.Title.CompareTo(p.Title);
+            else
+                throw new Exception("Невозможно сравнить!");
+        }
+        public void SearchBook(int code)
+        {
+            if (Code == code)
+            {
+                Console.WriteLine("Книга {0} - {1}, имеет код - {2}", Title, Name, Code);
+            }
+        }
+        public int checkCode(int code)
+        {
+            if (Code == code)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("\n------------------------------------------\nКод - " + this.Code + "\n");
+            sb.Append("Название книги - " + this.Title + "\n");
+            sb.Append("Ф.И.О. автора - " + this.Name + "\n");
+            sb.Append("Год написания - " + this.Year + "\n");
+            sb.Append("Жанр - " + this.Genre + "\n");
+            sb.Append("Количество страниц - " + this.NumPages + "\n");
+            return sb.ToString();
+        }
+    }
+}
---
+++ tpzslaba6(2)/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace tpzslaba6_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите количество книг: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                BookInLibrary[] book = new BookInLibrary[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("-------Книга # {0} ----------\n", i + 1);
+                    Console.Write("ФИО автора: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Название книги: ");
+                    string title = Console.ReadLine();
+                    Console.Write("Код: ");
+                    int code = int.Parse(Console.ReadLine());
+                    Console.Write("Год написания: ");
+                    int year = int.Parse(Console.ReadLine());
+                    Console.Write("Количество страниц: ");
+                    int numPages = int.Parse(Console.ReadLine());
+                    Console.Write("Жанр: ");
+                    string genre = Console.ReadLine();
+                    book[i] = new BookInLibrary(name, title, code, year, numPages, genre);
+                }
+                Array.Sort(book);
+                Console.WriteLine("\n\t\tНАЗВАНИЯ КНИГ СОРТИРОВАНЫ ПО АЛФАВИТУ");
+                foreach (BookInLibrary b in book)
+                {
+                    Console.WriteLine(b.ToString());
+                }
+                Console.WriteLine("Введите код книги для поиска: ");
+                int codeSeach = int.Parse(Console.ReadLine());
+                int check = 0;
+                foreach (BookInLibrary b1 in book)
+                {
+                    check = b1.checkCode(codeSeach);
+                }
+                if (check == 1)
+                {
+                    foreach (BookInLibrary b2 in book)
+                    {
+                        b2.SearchBook(codeSeach);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Книг с кодом " + codeSeach + " не найдено!");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка - " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
